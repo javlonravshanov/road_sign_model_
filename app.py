ed471bb400a59eec9cb62889e1b41b2f607cc302
Javlon Ravshanov <150927897+javlonravshanov@users.noreply.github.com>
2024-05-28 14:31:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
 
 # title
 st.write("""
